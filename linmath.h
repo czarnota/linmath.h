edae9f5d4a39d4d67df6392491bf57056ba54a3c
Przemyslaw Czarnota <p@czarnota.io>
2023-04-23 10:43:22 +0200

    my change

--- linmath.h
+++ linmath.h
@@ -31,6 +31,12 @@ LINMATH_H_FUNC void vec##n##_scale(vec##n r, vec##n const v, float const s) \
 	for(i=0; i<n; ++i) \
 		r[i] = v[i] * s; \
 } \
+LINMATH_H_FUNC void vec##n##_copy(vec##n r, vec##n const v, float const s) \
+{ \
+	int i; \
+	for(i=0; i<n; ++i) \
+		r[i] = v[i]; \
+} \
 LINMATH_H_FUNC float vec##n##_mul_inner(vec##n const a, vec##n const b) \
 { \
 	float p = 0.f; \
